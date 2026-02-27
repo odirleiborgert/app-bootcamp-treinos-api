90c9c28c565b119c525ac2678a5c7cf6ffeac739
Odirlei Borgert <odirleiborgert@gmail.com>
2026-02-27 18:23:27 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import "dotenv/config";
 
-const name = 'Odirlei';
-console.log("Hello World " + name);
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT ?? 3000) });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
